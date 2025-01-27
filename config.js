afff42724ecc2e68d5e0791a81d0aaab561360c5
MADARA <145380330+UchihaMadara07@users.noreply.github.com>
2025-01-27 05:35:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ module.exports = {
   TIMEZONE: process.env.TIMEZONE || "Asia/Kolkata",
   STARTING_MSG: toBool(process.env.STARTING_MSG || "true"),
   STATUS_REACTION_EMOJI: process.env.STATUS_REACTION_EMOJI || "🌼,♥️",
-  STATUS_REACTION: toBool(process.env.STATUS_REACTION || "false"),
+  STATUS_REACTION: toBool(process.env.STATUS_REACTION || "true"),
   GEMINI: process.env.GEMINI || "null",
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
